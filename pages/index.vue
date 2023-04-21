0a4d925c6b36c7052cb9d76cce3052579534c29f
ALUtech166 <s.tassiga@alustudent.com>
2023-04-22 01:11:42 +0200

    updates

---
+++ pages/index.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="bg-gray-200 min-h-screen">
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 mt-4">
+      <div class="flex items-center justify-between mb-4">
+        <h1 class="text-xl lg:text-4xl font-bold text-black">Image Dashboard</h1>
+        <div class="relative">
+          <input
+            type="text"
+            class="px-4 py-2 w-64 bg-white text-black rounded-full focus:outline-none"
+            placeholder="Search"
+          />
+          <svg
+            class="absolute right-0 top-0 mt-3 mr-4 h-4 w-4 fill-current text-gray-500"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path
+              d="M18.7 17.3l-4.8-4.8C14.6 11.6 15 10.4 15 9c0-3.9-3.1-7-7-7s-7 3.1-7 7 3.1 7 7 7c1.4 0 2.6-.4 3.7-1.1l4.8 4.8c.4.4 1 .4 1.4 0 .4-.4.4-1 0-1.4zM5 9c0-2.8 2.2-5 5-5s5 2.2 5 5-2.2 5-5 5-5-2.2-5-5z"
+            />
+          </svg>
+        </div>
+      </div>
+      <div class="mt-8 grid gap-6 grid-cols-1 sm:grid-cols-2 lg
+      :grid-cols-3 xl:grid-cols-4">
+      <div class="bg-black rounded-lg hover:shadow-2xl">
+        <img src="https://source.unsplash.com/random" alt="Random image" class="rounded-t-lg">
+        <div class="p-4">
+          <h2 class="text-white font-bold text-lg mb-2">Image Title</h2>
+          <p class="text-gray-400 text-sm mb-4">By John Doe</p>
+          <button class="bg-white text-black py-2 px-4 rounded">Download</button>
+        </div>
+      </div>
+      <div class="bg-black rounded-lg hover:shadow-2xl">
+        <img src="https://source.unsplash.com/random" alt="Random image" class="rounded-t-lg">
+        <div class="p-4">
+          <h2 class="text-white font-bold text-lg mb-2">Image Title</h2>
+          <p class="text-gray-400 text-sm mb-4">By John Doe</p>
+          <button class="bg-white text-black py-2 px-4 rounded">Download</button>
+        </div>
+      </div>
+      <div class="bg-black rounded-lg hover:shadow-2xl">
+        <img src="https://source.unsplash.com/random" alt="Random image" class="rounded-t-lg">
+        <div class="p-4">
+          <h2 class="text-white font-bold text-lg mb-2">Image Title</h2>
+          <p class="text-gray-400 text-sm mb-4">By John Doe</p>
+          <button class="bg-white text-black py-2 px-4 rounded">Download</button>
+        </div>
+      </div>
+      <div class="bg-black rounded-lg hover:shadow-2xl">
+        <img src="https://source.unsplash.com/random" alt="Random image" class="rounded-t-lg">
+        <div class="p-4">
+          <h2 class="text-white font-bold text-lg mb-2">Image Title</h2>
+          <p class="text-gray-400 text-sm mb-4">By John Doe</p>
+          <button class="bg-white text-black py-2 px-4 rounded">Download</button>
+        </div>
+      </div>
+    </div>
+
+
+
+  <div class="mt-8 grid gap-6 grid-cols-1 sm:grid-cols-2 lg
+      :grid-cols-3 xl:grid-cols-4">
+      <div class="bg-black rounded-lg hover:shadow-2xl">
+        <img src="https://source.unsplash.com/random" alt="Random image" class="rounded-t-lg">
+        <div class="p-4">
+          <h2 class="text-white font-bold text-lg mb-2">Image Title</h2>
+          <p class="text-gray-400 text-sm mb-4">By John Doe</p>
+          <button class="bg-white text-black py-2 px-4 rounded">Download</button>
+        </div>
+      </div>
+      <div class="bg-black rounded-lg hover:shadow-2xl">
+        <img src="https://source.unsplash.com/random" alt="Random image" class="rounded-t-lg">
+        <div class="p-4">
+          <h2 class="text-white font-bold text-lg mb-2">Image Title</h2>
+          <p class="text-gray-400 text-sm mb-4">By John Doe</p>
+          <button class="bg-white text-black py-2 px-4 rounded">Download</button>
+        </div>
+      </div>
+      <div class="bg-black rounded-lg hover:shadow-2xl">
+        <img src="https://source.unsplash.com/random" alt="Random image" class="rounded-t-lg">
+        <div class="p-4">
+          <h2 class="text-white font-bold text-lg mb-2">Image Title</h2>
+          <p class="text-gray-400 text-sm mb-4">By John Doe</p>
+          <button class="bg-white text-black py-2 px-4 rounded">Download</button>
+        </div>
+      </div>
+      <div class="bg-black rounded-lg hover:shadow-2xl">
+        <img src="https://source.unsplash.com/random" alt="Random image" class="rounded-t-lg">
+        <div class="p-4">
+          <h2 class="text-white font-bold text-lg mb-2">Image Title</h2>
+          <p class="text-gray-400 text-sm mb-4">By John Doe</p>
+          <button class="bg-white text-black py-2 px-4 rounded">Download</button>
+        </div>
+      </div>
+    </div>
+
+
+    <footer class="text-white text-center font-bold bg-black mt-8 rounded italic">
+      <div class="py-4 text-center px-24 items-center">
+        <div class="lg:mb-0">
+          <div class="flex flex-row gap-2 justify-center">
+            <span>Copyright {{ new Date().getFullYear() }} Image Dashboard </span>
+            <span class="text-center">Designed by Serge Tassiga</span>
+          </div>
+        </div>
+       
+      </div>
+    </footer>
+  
+</div>
+
+</div>
+</template>
